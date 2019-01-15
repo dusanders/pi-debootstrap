74d079f669004a73db71d6160d2b623c32bf70d8
Dustin Anderson <dusanders@gmail.com>
2019-01-15 05:04:34 -0800

    resolve runtime error

--- 03-kernel-compile.sh
+++ 03-kernel-compile.sh
@@ -30,18 +30,21 @@ function Exit()
 ## 
 function GetVars() 
 {
-	if [ -! -e "${CONFIG_SCRIPT}" ]; then
+	if [ ! -e "${CONFIG_SCRIPT}" ]; then
 		Exit "Failed to find config script - Exiting..."
 	fi
-	CROSS_COMPILER_DEST=$(./${CONFIG_SCRIPT} CROSS_COMPILER_GIT) || Exit "Failed to get cross compiler location"
-	FIRMWARE_DEST=$(./${CONFIG_SCRIPT} FIRMWARE_GIT) || Exit "Failed to get firmware location"
-	KERNEL_DEST=$(./${CONFIG_SCRIPT} KERNEL_GIT) || Exit "Failed to get kernel location"
+	CROSS_COMPILER_DEST=$(./${CONFIG_SCRIPT} CROSS_COMPILER_DEST) || Exit "Failed to get cross compiler location"
+	FIRMWARE_DEST=$(./${CONFIG_SCRIPT} FIRMWARE_DEST) || Exit "Failed to get firmware location"
+	KERNEL_DEST=$(./${CONFIG_SCRIPT} KERNEL_DEST) || Exit "Failed to get kernel location"
     KERNEL_ARG=$(./${CONFIG_SCRIPT} KERNEL_ARG) || Exit "Failed to get kernel argument"
     DEFCONFIG=$(./${CONFIG_SCRIPT} DEFCONFIG) || Exit "Failed to get kernel defconfig"
     J_OPTION=$(./${CONFIG_SCRIPT} J_OPTION) || Exit "Failed to get 'j' option"
     ARCH_TYPE=$(./${CONFIG_SCRIPT} ARCH_TYPE) || Exit "Failed to get ARCH type"
     COMPILE_PREFIX=$(./${CONFIG_SCRIPT} CROSS_COMPILE_PREFIX) || Exit "Failed to get cross compile prefix"
 }
+
+# Setup the variables
+GetVars
 
 # Move to kernel source
 cd "${KERNEL_DEST}"
